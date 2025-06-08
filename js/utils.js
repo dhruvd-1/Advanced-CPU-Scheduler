c5e611325c0e11d658744a3f137f91733a726648
Dhruv Dhankher <156188075+dhruvd-1@users.noreply.github.com>
2025-06-09 01:19:19 +0530

    Create utils.js

---
+++ js/utils.js
@@ -0,0 +1,16 @@
+// Common utility functions
+function debounce(func, wait) { /* ... */ }
+function throttle(func, limit) { /* ... */ }
+function formatTime(time) { /* ... */ }
+function generateUID() { /* ... */ }
+function validateEmail(email) { /* ... */ }
+function saveToLocalStorage(key, data) { /* ... */ }
+function loadFromLocalStorage(key) { /* ... */ }
+
+// Math utilities
+function calculateStandardDeviation(values) { /* ... */ }
+function findOptimalValue(data, metric) { /* ... */ }
+
+// DOM utilities
+function createElement(tag, className, content) { /* ... */ }
+function addEventListeners(elements, events) { /* ... */ }
